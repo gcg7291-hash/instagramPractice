fde8b330b7f6599330501cc3f043cfe9a11d36a0
gcg7291-hash <gcg7291@gmail.com>
2025-12-03 09:06:43 +0900

    post read all

--- src/main/java/com/example/instagram/security/CustomUserDetails.java
+++ src/main/java/com/example/instagram/security/CustomUserDetails.java
@@ -43,4 +43,8 @@ public class CustomUserDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public Long getId(){
+        return user.getId();
+    }
 }
--- src/main/java/com/example/instagram/service/UserService.java
+++ src/main/java/com/example/instagram/service/UserService.java
@@ -1,10 +1,12 @@
-package com.example.instagram.service;
+    package com.example.instagram.service;
 
-import com.example.instagram.dto.request.SignUpRequest;
-import com.example.instagram.entity.User;
+    import com.example.instagram.dto.request.SignUpRequest;
+    import com.example.instagram.entity.User;
 
-public interface UserService {
+    public interface UserService {
 
-    User register(SignUpRequest signUpRequest);
-    boolean existByUsername(String username);
-}
+        User register(SignUpRequest signUpRequest);
+        boolean existByUsername(String username);
+
+        User findById(Long userId);
+    }
